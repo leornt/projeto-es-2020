f4eb0ce28c243f45a0e5515a4b8e0f823eecd5f0
João Marcos Kaminoski de Souza <kamisou@outlook.com>
2021-04-21 15:46:24 -0300

    :lipstick: added custom 404 page

---
+++ www/resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+    <title>Not found</title>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+</head>
+
+<body style="width: 100vw; height: 100vh;">
+    <div style="display: flex; justify-content: center; height: 100%; align-items: center">
+        <span style="font-weight: bold; font-size: 24px; border-right: 2px solid #828282; padding-right: 10px;">404</span>
+        <span style="font-weight: bold; font-size: 24px; padding-left: 10px;">Not Found</span>
+    </div>
+</body>
+
+</html>
